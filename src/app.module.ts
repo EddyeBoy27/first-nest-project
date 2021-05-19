1f0c19a78d43a16ace61b6074349d59e8d6ef67d
eddyeboy27 <lipe0503@gmail.com>
2021-05-19 13:27:48 -0300

    Integração com mongodb concluida

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [TasksModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nestproject'),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
